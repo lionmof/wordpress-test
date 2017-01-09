e5fb0d8a6b8a049e084fe3f361f9f07a1bfb57d5
Sasha Bondar <sashbondar@yandex.ua>
2017-01-09 23:32:20 +0200

    Revert "test"
    This reverts commit 723fe35666be27a11a4b6113d8d8834bd37d28f5.

--- app/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// оставить  пустым, если в корне
-
-
-$config['db_host'] = "mysql.hostinger.com.ua";
-$config['db_user'] = "u326679603_u1";
-$config['db_password'] = "vi2237aa";
-$config['db_base'] = "u326679603_lol";
-$config['db_charset'] = "utf8";
